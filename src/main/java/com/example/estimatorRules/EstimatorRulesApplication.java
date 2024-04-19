87a9a23508412ff02d3a2a3545ceffb842ddc426
Louis Lu <louisyulu@gmail.com>
2024-04-19 14:19:35 -0400

    initial update

---
+++ src/main/java/com/example/estimatorRules/EstimatorRulesApplication.java
@@ -0,0 +1,13 @@
+package com.example.estimatorRules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstimatorRulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstimatorRulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/estimatorRules/EstimatorRulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estimatorRules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstimatorRulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
