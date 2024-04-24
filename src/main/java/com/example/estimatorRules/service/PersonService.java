70645cd7bc590afa9c57b0f3273e629072308262
Louis Lu <louisyulu@gmail.com>
2024-04-24 11:44:36 -0400

    remove drools engine

--- src/main/java/com/example/estimatorRules/service/PersonService.java
+++ src/main/java/com/example/estimatorRules/service/PersonService.java
@@ -1,25 +1,17 @@
 package com.example.estimatorRules.service;
 
-import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.KieSession;
 import org.springframework.stereotype.Service;
-
 import com.example.estimatorRules.model.Person;
 
 @Service
 public class PersonService {
 
-    private final KieContainer kieContainer;
-
-    public PersonService(KieContainer kieContainer) {
-        this.kieContainer = kieContainer;
-    }
-
     public Person evaluatePerson(Person person) {
-        KieSession kieSession = kieContainer.newKieSession();
-        kieSession.insert(person);
-        kieSession.fireAllRules();
-        kieSession.dispose();
+        if (person.getAge() >= 18) {
+            person.setAdult(true);
+        } else {
+            person.setAdult(false);
+        }
         return person;
     }
 }
